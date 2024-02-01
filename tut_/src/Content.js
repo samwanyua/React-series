dcadeaf18dfa3594c0d75c82bc8a48dc6eb0e333
samwanyua <samexample8@gmail.com>
2024-02-01 13:30:27 +0300

    useState Hook

--- tut_/src/Content.js
+++ tut_/src/Content.js
@@ -1,31 +1,39 @@
-const Content = () => {
-    const handleNameChange = () => { //add the 'handle' before the function name
-        const names = ['Bob', 'Einstein','Halaand','Willian', 'Drogba','Fofana']
-        const int = Math.floor(Math.random()* 6)
-        return names[int]
-    }
-    
-    const handleClick = () => {
-      console.log('You clicked it!')
-    }
-    // adding a parameter  - on the button add an anonymous function         <button onClick={() => handleClick2('Wanyua')}>Click it</button>
+import { useState } from "react"
 
-    const handleClick2 = (name) => {
-      console.log(`${name} was clicked`)
-    }
-    //adding an event
-    const handleClick3 = (e) => {
-      console.log(e.target.innerText)
-    }
+const Content = () => {
+const [name, setName] = useState('Wanyua') //default state
+
+const [count, setCount] = useState(0);
+
+const handleNameChange = () => { //add the 'handle' before the function name
+    const names = ['Bob', 'Einstein','Halaand','Willian', 'Drogba','Fofana']
+    const int = Math.floor(Math.random()* 6)
+    setName(names[int]); //setting new name as a state
+}
+
+const handleClick = () => {
+  setCount(count + 1)
+  console.log(count)
+}
+
+// adding a parameter  - on the button add an anonymous function         <button onClick={() => handleClick2('Wanyua')}>Click it</button>
+const handleClick2 = (name) => {
+  console.log(`${name} was clicked`)
+}
+
+  //adding an event
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText)
+  }
 
 
   return (
     <main>
         <p onDoubleClick={handleClick}>
-            Hello {handleNameChange()}
+            Hello {name}
         </p>
+        <button onClick={handleNameChange}>Change Name</button>
         <button onClick={handleClick}>Click it</button>
-        <button onClick={() => handleClick2('Wanyua')}>Click it</button>
         <button onClick={(e) => handleClick3(e)}>Click it</button>
 
         
